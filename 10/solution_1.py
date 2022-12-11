e38e850ffd2cf5c64785acad5939471659b832b3
wolfram74 <phaugen@gmail.com>
2022-12-10 20:56:28 -0600

    was not actually calculating total

--- 10/solution_1.py
+++ 10/solution_1.py
@@ -12,20 +12,23 @@ def reconstruct_reg_history(commands):
         command = commands.readline().rstrip()
     return register
 
+
 def signal_strength_calculator(x_reg):
     total = 0
     check = 0
     sample_index = check*40+20-1
     while sample_index < len(x_reg):
-        total = x_reg[sample_index]*(sample_index+1)
+        total += x_reg[sample_index]*(sample_index+1)
         print(check, sample_index, total)
+        print()
         check+=1
         sample_index = check*40+20-1
+    print(total)
     pass
 
 if __name__ == '__main__':
     with open('input.txt', 'r') as command_log:
-    # with open('test_input.txt', 'r') as command_log:
+    # with open('test_input.txt', 'r') as command_log: #13140 result
 
         x_reg_history = reconstruct_reg_history(command_log)
         print(x_reg_history[:10])
