8014adc43353589ef741145a1878276825895a05
wolfram74 <phaugen@gmail.com>
2022-12-13 09:34:42 -0600

    answer is too high

---
+++ 11/solution_1.py
@@ -0,0 +1,99 @@
+
+class Monkey():
+    def __init__(self, 
+        ID_num, items, operation, test_num, recipients
+        ):
+        self.ID_num = ID_num
+        self.items = items
+        self.operation = operation
+        self.test_num = test_num
+        self.recipients = recipients
+        self.monkey_level = 0
+
+    def evaluate_items(self):
+        self.truthy = []
+        self.falsey = []
+        self.monkey_level+= len(self.items)
+        while self.items:
+            current = self.items.pop(0)
+            new_val = round( self.operation(current)/3 )
+            if new_val%self.test_num == 0:
+                self.truthy.append(new_val)
+                continue
+            self.falsey.append(new_val)
+
+def build_monkeys(census):
+    line = census.readline()
+    monkey_entry = []
+    monkeys = []
+    while line:
+        monkey_entry.append(line.rstrip())
+        # print(line)
+        line = census.readline()
+        if line == '\n':
+            monkeys.append(parse_entry(monkey_entry))
+            line = census.readline()
+            monkey_entry = []
+    monkeys.append(parse_entry(monkey_entry))
+    return monkeys
+
+def parse_entry(lines):
+    ID_num = lines[0][-2]
+    
+    items = lines[1].split(':')[1].split(',')
+    items = [int(el) for el in items]
+
+    if '+' in lines[2]:
+        operation = lambda x: x + int(lines[2][-1])
+    else:
+        if lines[2][-3:] == 'old':
+            print(ID_num)
+            operation = lambda x: x**2
+        else:
+            operation = lambda x: x * int(lines[2][-1])
+
+
+    test_num = int(lines[3].split('by')[-1])
+
+    recipients = [
+        int(lines[4][-1]),
+        int(lines[5][-1]),
+        ]
+
+    # for line in lines:
+    #     print(line)
+    # print(ID_num, items, operation, test_num, recipients)
+    # print('----')
+    return Monkey(ID_num=ID_num, items=items, operation=operation, test_num=test_num, recipients=recipients)
+
+def do_round(monkeys):
+    for monkey in monkeys:
+        monkey.evaluate_items()
+        monkeys[monkey.recipients[0]].items += monkey.truthy
+        monkeys[monkey.recipients[1]].items += monkey.falsey
+    return monkeys
+
+def inspect_items(monkeys):
+    for monkey in monkeys:
+        print(monkey.items)
+    print('----')
+
+def find_monkey_business(monkeys):
+    monkey_levels = sorted([monkey.monkey_level for monkey in monkeys])
+    print(monkey_levels[-1]*monkey_levels[-2])
+
+
+if __name__ == '__main__':
+    with open('input.txt', 'r') as monkey_census: #
+    # with open('test_input.txt', 'r') as command_log: #13140 result
+
+        monkeys = build_monkeys(monkey_census)
+        # monkeys[0].evaluate_items()
+        # print(monkeys[0].truthy)
+        # print(monkeys[0].falsey)
+        for loop in range(20):
+            # inspect_items(monkeys)
+            monkeys = do_round(monkeys)
+        inspect_items(monkeys)
+        find_monkey_business(monkeys) # 91760 is too high
+
