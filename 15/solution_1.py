d9c84a959d33c354647258a84d911b2cde3d61f2
wolfram74 <phaugen@gmail.com>
2022-12-17 11:46:18 -0600

    prematurely constraining search space was a mistake

--- 15/solution_1.py
+++ 15/solution_1.py
@@ -41,7 +41,7 @@ def parse_input(file):
 def rule_outs(beacon_pairs, x_bounds, row_check):
     row = set(range(x_bounds[0],x_bounds[1]+1))
     print(len(row))
-    print(row)
+    # print(row)
     rejects = []
     # print(row)
     for pair in beacon_pairs:
@@ -54,7 +54,8 @@ def rule_outs(beacon_pairs, x_bounds, row_check):
             continue
         # print(sens, 'care about it')
         x_budget = distance - delta_y
-        # print(sens, distance ,x_budget)
+        print(sens, distance ,x_budget)
+        print(sens[0]-x_budget, sens[0]+x_budget+1)
         for check in range(sens[0]-x_budget, sens[0]+x_budget+1):
             # print(check)
             # if check%1000 == 0:
@@ -68,6 +69,69 @@ def rule_outs(beacon_pairs, x_bounds, row_check):
 
     # return 5
     return len(rejects)
+
+def composite_range_builder(beacon_pairs, x_bounds, row_check):
+    ranges = []
+    for pair in beacon_pairs:
+        sens = pair[0]
+        beac = pair[1]
+        distance = mh_metric(sens,beac)
+        delta_y = abs(sens[1]-row_check)
+        if delta_y > distance:
+            # print(sens, 'too far to matter')
+            continue
+        # print(sens, 'care about it')
+        x_budget = distance - delta_y
+        # print(sens, distance ,x_budget)
+        # print(sens[0]-x_budget, sens[0]+x_budget)
+        sensor_range = [sens[0]-x_budget, sens[0]+x_budget]
+        stitched = False
+        for index, bounds in enumerate(ranges):
+            if not overlap_check(bounds, sensor_range):
+                continue
+            ranges[index] = stitch_ranges(bounds, sensor_range)
+            stitched = True
+            break
+        if not stitched:
+            ranges.append(sensor_range)
+    output = []
+    while not len(output)==1:
+        for range0 in ranges:
+            stitched = False
+            for index, bounds in enumerate(output):
+                if not overlap_check(bounds, range0):
+                    continue
+                output[index] = stitch_ranges(bounds, range0)
+                stitched = True
+                break
+            if not stitched:
+                output.append(range0)
+        ranges = output
+        print(ranges)
+        if len(output) == 1:
+            break
+        output = []
+
+    print(ranges)
+
+def inside_range(bounds, value):
+    if bounds[0] > value:
+        return False
+    if bounds[1] < value:
+        return False
+    return True
+
+def overlap_check(range1, range2):
+    if inside_range(range1, range2[0]):
+        return True
+    if inside_range(range1, range2[1]):
+        return True
+    if inside_range(range2, range1[1]):
+        return True
+    return False
+
+def stitch_ranges(range1, range2):
+    return [min(range1[0],range2[0]), max(range1[1],range2[1])]
 
 def beacon_check(pairs, row):
     output = 0
@@ -93,21 +157,27 @@ if __name__ == '__main__':
         # print(mh_metric(sensor_beacon_pairs[0][0], sensor_beacon_pairs[0][1]))
         rejects = rule_outs(sensor_beacon_pairs, space[0], row_query)
         beacons_in_row = beacon_check(sensor_beacon_pairs, row_query)
-        print(rejects, beacons_in_row)
+        composite_range_builder(sensor_beacon_pairs, space[0], row_query)
+        # print(rejects, beacons_in_row)
         total = rejects-beacons_in_row
-        print(total, total==26)
+        # total = 7
+        # print(total, total==26)
         if not total==26:
             exit()
-    exit()
+    # exit()
     with open('input.txt', 'r') as sensor_positions: # 
         print('going to the big leagues')
         sensor_beacon_pairs, space = parse_input(sensor_positions)
         print(space)
         row_query = 2000000
-        rejects = rule_outs(sensor_beacon_pairs, space[0], row_query)
-        beacons_in_row = beacon_check(sensor_beacon_pairs, row_query)
-        print(rejects, beacons_in_row)
-        total = rejects-beacons_in_row
-        print(total) #4406715 is too low
+        composite_range_builder(sensor_beacon_pairs, space[0], row_query)
+
+        # rejects = rule_outs(sensor_beacon_pairs, space[0], row_query)
+        # beacons_in_row = beacon_check(sensor_beacon_pairs, row_query)
+        # print(rejects, beacons_in_row)
+        # total = rejects-beacons_in_row
+        # print(total) #4406715 is too low
+        # synthetic bound approach also yields 4406715
+        # not limiting space to places where things already are gets 5367037 and the right answer
 
 
