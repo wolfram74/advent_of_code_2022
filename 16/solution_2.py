c90d621668871681168b77e100d493f785239f1f
wolfram74 <phaugen@gmail.com>
2022-12-18 14:33:20 -0600

    modifications give new wrong answer even at maximum branching

--- 16/solution_2.py
+++ 16/solution_2.py
@@ -47,30 +47,39 @@ class Plan:
             if valve.flow > 0:
                 self.flowing[key] = valve.flow
 
-    def best_next(self, branching_factor, plan):
+    def best_next(self, branching_factor, plan, debug=False):
         # first pass at trying to have bart and stampy team up
 
         bart = self.valves[plan[-1][1]]
         stampy = self.valves[plan[-1][3]]
         bart_time = self.total_time-plan[-1][0]
-        stampy_time = self.total_time-plan[-1][0]
+        stampy_time = self.total_time-plan[-1][2]
+        valve_set = set(self.flowing.keys())
+        bart_set = set([el[1] for el in plan])
+        stampy_set = set([el[3] for el in plan])
         unvisited = (
-            set(self.flowing.keys()) 
-            - set([el[1] for el in plan]) # bart's valves
-            - set([el[3] for el in plan]) # stampy's valves
+            (valve_set-bart_set)-stampy_set
             )
+        if debug:
+            print(valve_set)
+            print(bart_set, stampy_set)
+            print(valve_set-bart_set)
         # in 1 player options were a 2-item pressure value, destination, 
         # maybe 2 player is a 3 item, pressure value, bart destination, stampy destination
         # only one of which is changed? so each candidate generates two options
         options = []
         for candidate in unvisited:
-            # for index, actor in enumerate([bart, stampy]):
-            #     # print(candidate)
-            #     # print(origin.paths[candidate])
+            if debug:
+                print(candidate)
             delta_t_b = 1+len(bart.paths[candidate])
             delta_t_s = 1+len(stampy.paths[candidate])
             value_b = self.flowing[candidate]*(bart_time-delta_t_b)
             value_s = self.flowing[candidate]*(stampy_time-delta_t_s)
+            if debug:
+                print('---')
+                print(bart_time, stampy_time)
+                print(delta_t_b, delta_t_s)
+                print(value_b, value_s)
             if value_b > 0:
                 options.append([value_b, candidate, stampy.ID])
             if value_s > 0:
@@ -79,7 +88,7 @@ class Plan:
         # print(options)
         return options[:branching_factor]
 
-    def advance_step(self, branching_factor):
+    def advance_step(self, branching_factor, debug=False):
         new_plans = []
         for plan in self.plans:
             bart = self.valves[plan[-1][1]]
@@ -111,9 +120,12 @@ class Plan:
                     self.finished_plans.append(new_plans.pop())
         self.plans = new_plans
 
-    def rank_plans(self):
+    def rank_plans(self, final=False):
+        if final:
+            self.finished_plans = list(sorted(self.finished_plans, reverse=True, key=self.evaluate_plan))
+            return
         self.plans = list(sorted(self.plans, reverse=True, key=self.evaluate_plan))
-        self.finished_plans = list(sorted(self.finished_plans, reverse=True, key=self.evaluate_plan))
+        return
 
     def evaluate_plan(self, plan):
         pressure = 0
@@ -130,6 +142,29 @@ class Plan:
                 pressure += delta_t_s*self.valves[valve_s].flow
                 accounted[valve_s] = pressure
         return pressure
+
+    def decile_report(self):
+        contenders = len(self.finished_plans)
+        for i in range(10):
+            index = int(contenders*i/10)
+            print(i, index,'---',self.evaluate_plan(self.finished_plans[index]))
+        return
+
+    def prune_bottom_two_thirds(self):
+        contenders = len(self.plans)
+        keep_index = int(contenders/3)
+        self.rank_plans()
+        self.plans = self.plans[:keep_index]
+        return
+
+    def prune_best_finished(self):
+        # contenders = 
+        if len(self.finished_plans) == 0:
+            return
+        # keep_index = int(contenders/3)
+        self.rank_plans(final=True)
+        self.finished_plans = [self.finished_plans[0]]
+        return
 
 
 def parse_file(file_name):
@@ -181,24 +216,33 @@ def solution(file_name):
     #     exit()
     # print(plan.best_next(branching_factor = 3, path = plan.paths[0]))
     steps = 0
-    # while plan_coordinator.plans:
-    for i in range(7):
-        plan_coordinator.advance_step(branching_factor=8)
+    while plan_coordinator.plans:
+    # for i in range(7):
+        plan_coordinator.advance_step(branching_factor=15)
         steps += 1
-        print(steps, len(plan_coordinator.plans))
-    plan_coordinator.rank_plans()
-    print('time left')
+        print('%02d' % steps, len(plan_coordinator.plans), len(plan_coordinator.finished_plans))
+        if len(plan_coordinator.plans) > 10**6:
+            plan_coordinator.prune_bottom_two_thirds()
+        plan_coordinator.prune_best_finished()
+        # print(steps, len(plan_coordinator.plans), len(plan_coordinator.finished_plans))
+    plan_coordinator.rank_plans(final=True)
+
+    # print('time left')
     # for plan in plan_coordinator.plans:
     #     # print(plan_coordinator.evaluate_plan(plan), plan)
     #     print(plan_coordinator.evaluate_plan(plan))
     # print(plan_coordinator.plans)
-    print('time up')
+
+    # print('time up')
     # for plan in plan_coordinator.finished_plans:
     #     # print(plan_coordinator.evaluate_plan(plan), plan)
     #     print(plan_coordinator.evaluate_plan(plan))
     # print(len(plan_coordinator.plans), len(plan_coordinator.finished_plans))
-    print(plan_coordinator.finished_plans[0])
-    return plan_coordinator.evaluate_plan(plan_coordinator.finished_plans[0])
+    # print(plan_coordinator.finished_plans[0])
+    winner = plan_coordinator.finished_plans[0]
+    # plan_coordinator.decile_report()
+    # plan_coordinator.best_next(branching_factor=8, plan = winner, debug=True)
+    return plan_coordinator.evaluate_plan(winner)
 
 def print_distance_matrix(valves):
     keys = list(valves.keys())
@@ -229,21 +273,18 @@ if __name__ == '__main__':
     # exit()
     print(solution('input.txt'))
     # part 1 best 1896
-    # bf 5 -> 1794
-    # bf 6 -> 1803
-    # bf 7 -> 1828
-    # plan = [(0, 'AA', 0, 'AA'), (5, 'JO', 0, 'AA'), (9, 'KP', 0, 'AA'), (9, 'KP', 7, 'EV'), (9, 'KP', 11, 'FB'), (16, 'CN', 11, 'FB'), (19, 'HB', 11, 'FB')]
-    # bf 8 -> 1828
-    # plan = [(0, 'AA', 0, 'AA'), (5, 'JO', 0, 'AA'), (9, 'KP', 0, 'AA'), (9, 'KP', 7, 'EV'), (9, 'KP', 11, 'FB'), (16, 'CN', 11, 'FB'), (19, 'HB', 11, 'FB')]
 
-    # bf 9 -> 1828 too low
-    # bf 10 -> 1828 
-    # bf 11 -> 1828 X
-    # bf 12 -> 1828 X
-    # bf 13 -> 1828 x
-    # bf 14 -> 1828 x
-    # bf 15 -> 1828 god damn it
-
+    # bf 5 -> 2320
+    # bf 6 -> 2320
+    # bf 7 -> 2339 x
+    # bf 8 -> 2339 x
+    # bf 9 -> 2380 wrong
+    # bf 10 -> 2402
+    # bf 11 -> 2483
+    # bf 12 -> 2483 wrong
+    # bf 13 -> 2483 x
+    # bf 14 -> 2483 x
+    # bf 15 -> 2339
 '''
 1 14
 2 196
@@ -261,5 +302,43 @@ if __name__ == '__main__':
 6 6057606
 7 61493639
 
+01 15 0
+02 225 0
+03 3375 0
+04 50304 0
+05 682699 0
+06 6787579 8037
+07 17090968 52738
+
 '''
 
+
+# bad time keeping for stampy/vs part
+# bf 5 -> 1794
+# bf 6 -> 1803
+# bf 7 -> 1828
+# plan = [(0, 'AA', 0, 'AA'), (5, 'JO', 0, 'AA'), (9, 'KP', 0, 'AA'), (9, 'KP', 7, 'EV'), (9, 'KP', 11, 'FB'), (16, 'CN', 11, 'FB'), (19, 'HB', 11, 'FB')]
+# bf 8 -> 1828
+# plan = [(0, 'AA', 0, 'AA'), (5, 'JO', 0, 'AA'), (9, 'KP', 0, 'AA'), (9, 'KP', 7, 'EV'), (9, 'KP', 11, 'FB'), (16, 'CN', 11, 'FB'), (19, 'HB', 11, 'FB')]
+
+# bf 9 -> 1828 too low
+# bf 10 -> 1828 
+# bf 11 -> 1828 X
+# bf 12 -> 1828 X
+# bf 13 -> 1828 x
+# bf 14 -> 1828 x
+# bf 15 -> 1828 god damn it
+
+# hadn't disabled premature exit after 7 loops
+# bf 4 -> 1641
+# bf 5 -> 1655
+# bf 6 -> 1655
+# bf 7 -> 1685
+# bf 8 -> 1685
+# bf 9 -> 1685 x
+# bf 10 -> 1685 x
+# bf 11 -> 1685 x
+# bf 12 -> 1685 x
+# bf 13 -> 1685
+# bf 14 -> 1685
+# bf 15 -> 1685
