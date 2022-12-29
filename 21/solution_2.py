33a5765106b1f6380b4f8de6e6d80d4a2d0c6b8f
wolfram74 <phaugen@gmail.com>
2022-12-29 14:02:29 -0600

    using floats isntead of ints actually gave a higher wrong answer

--- 21/solution_2.py
+++ 21/solution_2.py
@@ -54,7 +54,8 @@ operations = {
     '+': (lambda x,y: x+y),
     '-': (lambda x,y: x-y),
     '*': (lambda x,y: x*y),
-    '/': (lambda x,y: int(x/y)),
+    # '/': (lambda x,y: int(x/y)),
+    '/': (lambda x,y: x/y),
 }
 
 inverse_ops = {
@@ -64,34 +65,47 @@ inverse_ops = {
     '/':'*'
 }
 def collapse_tree(expression_tree):
-    output = 0
+    # expression tree has either two leaves, two trees or one tree and one leaf
+    # or it is a leaf itself, immediate return
+    if expression_tree == 'humn':
+        print('oh ho, base case variable')
+        return expression_tree
+    if type(expression_tree) in (int, float):
+        return expression_tree
+
     x = expression_tree[0]
     y = expression_tree[2]
     op = expression_tree[1]
     weirdness = False
-    # if x == 2:
-    #     print('hey, its happening', expression_tree)
-    #     weirdness = True
+    # because leaves are handled as base cases just feed things into collapse again
+    x = collapse_tree(x)
+    y = collapse_tree(y)
+
     if x == 'humn' or y == 'humn':
+        if type(x) == list:
+            x = collapse_tree(x)
+        if type(y) == list:
+            y = collapse_tree(y)
         print(expression_tree, 'turned into \n', x, op, y)
-        return expression_tree
-    if type(x)==list:
-        x = collapse_tree(x)
-    if type(y)==list:
-        y = collapse_tree(y)
+        print( type(expression_tree), type([x, op, y]))
+        return [x, op, y]
+    if x == 'humn' or y == 'humn':
+        print('should have returned by now because of humn')
+
     if type(x)==list or type(y)==list:
-        return expression_tree
+        # print('farts', expression_tree)
+        return [x, op, y]
     try:
         return operations[op](x, y)
     except:
         if weirdness:
             print('weird', x, y)
-        print(expression_tree, 'turned into \n', x, op, y)
+        # print(expression_tree, 'turned into \n', x, op, y)
         return([x, op, y])
         exit()
 
 def trim_dowel(left_exp, right_exp):
-    if type(left_exp) == int:
+    if type(left_exp) in (int, float):
         output = left_exp
         dowel = right_exp
     else:
@@ -101,14 +115,29 @@ def trim_dowel(left_exp, right_exp):
     while type(dowel) == list:
         operation = dowel[1]
         # print(dowel)
-        if type(dowel[0])==int:
+        if type(dowel[0]) in (int, float):
             number = dowel[0]
             dowel = dowel[2]
         else:
             number = dowel[2]
             dowel = dowel[0]
-        print(output, number)
-        output = operations[inverse_ops[operation]](output, number)
+            # if number == 'humn':
+            #     temp_num = collapse_tree(dowel)
+            #     dowel, number = number, temp_num
+            # if dowel == 'humn':
+            #     number = collapse_tree(number)
+
+        if 'humn' in (number, dowel):
+            print('fudge baskets', number, operation, dowel)
+        try:
+            print(output, 'invert', operation, number)
+            output = operations[inverse_ops[operation]](output, number)
+        except :
+            print('!!!! aggh')
+            print(output, operation)
+            print('dowel', dowel)
+            print('number', number)
+            exit()
     print(output,dowel)
 
     return output
@@ -126,15 +155,20 @@ def solution(file_name):
     #     expression_dict['root'][index] = expression_dict[term]
     lookups = True
     # for i in range(6):
+    layers = 1
     while lookups:
         expression_dict['root'], lookups = expand_list(expression_dict['root'], expression_dict)
-        print(lookups)
+        # print(lookups)
+        layers+=1
+    print('layers', layers)
     # print(len(expression_dict['root']))
     # print(expression_dict['root'])
     left_value = collapse_tree(expression_dict['root'][0])
+    print(expression_dict['root'][0])
+    print(left_value, 'LLLLLL')
     right_value = collapse_tree(expression_dict['root'][2])
-    print(left_value, 'L')
-    print(right_value, 'R')
+    print(expression_dict['root'][2])
+    print(right_value, 'RRRRRR')
     result = trim_dowel(left_value, right_value)
     return result
 
@@ -144,8 +178,27 @@ if __name__ == '__main__':
         print('test failed, stopping')
         exit()
 
-    print('test passing, onto full')
+    if not solution('test_input2.txt') == 301:
+        print('test failed, stopping')
+        exit()
+    if not solution('test_input3.txt') == 301:
+        print('test failed, stopping')
+        exit()
+    print('test passing, onto full-----------')
     # exit()
     print(solution('input.txt'))
-    # part 1 4310
-    # 4040 too high
+    # part 2 3949235418274 is too high
+             # 3949235418274
+             # 3949235418274
+             # 3949235418276.4946
+'''
+weird_nested_list
+element = weird_nest_list[0]
+layer= 1
+while type(element)==list:
+    element = element[0]
+    layer+=1
+
+
+
+'''
